a3a0b123d55c44ecd9d263fb02bc900bffb8bc61
hyangteng <hyangteng@gmail.com>
2024-06-06 22:31:54 +0800

    fix: safari style bug with nest css

--- src/app/home/components/Header.tsx
+++ src/app/home/components/Header.tsx
@@ -1,31 +1,26 @@
-import Link from "next/link";
-import {GEEK_TIME_ARTICLES_LINK, NOTION_LINK} from "@/app/home/data";
+import Link from 'next/link'
+import { GEEK_TIME_ARTICLES_LINK, NOTION_LINK } from '@/app/home/data'
 
 export default function Header() {
-  let list = [
-    { path: "/english", name: "English Learning" },
-    { path: GEEK_TIME_ARTICLES_LINK, name: "Programming Learning" },
-    { path: NOTION_LINK, name: "Knowledge Architecture" },
-  ];
-  return (
-    <div className="home-header">
-      <div className="home-header-inner">
-        <div className="header-left">
-          <Link href={"/"}>
-            <img src={"/logo.png"} alt="logo" />
-          </Link>
-        </div>
-        <div className="header-right">
-          <div className="link-list">
-            {list.map((item) => (
-                <Link href={item.path} key={item.name}>
-                  {item.name}
+    let list = [
+        { path: '/english', name: 'English Learning' },
+        { path: GEEK_TIME_ARTICLES_LINK, name: 'Programming Learning' },
+        { path: NOTION_LINK, name: 'Knowledge Architecture' },
+    ]
+    return (
+        <div className="home-header">
+            <div className="home-header-inner">
+                <Link href={'/'}>
+                    <img src={'/logo.png'} alt="logo" />
                 </Link>
-            ))}
-          </div>
-
+                <div className="link-list">
+                    {list.map((item) => (
+                        <Link href={item.path} key={item.name}>
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
