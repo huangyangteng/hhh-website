35cbd02dd767b8bb589dac36fa18c5e7c1e9226e
hyangteng <hyangteng@gmail.com>
2024-06-11 23:22:08 +0800

    fix: keep query params

--- src/app/english/peppa/_components/PeppaList.tsx
+++ src/app/english/peppa/_components/PeppaList.tsx
@@ -2,19 +2,23 @@
 import { usePeppaVideos } from '@/app/english/apis'
 import { Pagination, Skeleton } from 'antd'
 import { useEffect, useState } from 'react'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 export default function PeppaList() {
     const { isLoading, data } = usePeppaVideos()
+    const search = useSearchParams()
     const total = data ? data.pages.length : 1
-    const [current, setCurrent] = useState(1)
+    const [current, setCurrent] = useState(
+        search.get('page') ? parseInt(search.get('page')) : 1,
+    )
     const router = useRouter()
     const pathname = usePathname()
-
     useEffect(() => {
         if (!data) return
         let cur = data.pages[current - 1]
-        router.push(`${pathname}?vid=${cur.bvid}`)
+        router.push(
+            `${pathname}?vid=${cur.bvid}&page=${current}&edit=${search.get('edit')}`,
+        )
     }, [data, current])
     return (
         <div className="peppa-navbar">
