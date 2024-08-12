e8ef449a7c35b14f693400f8b0db567ca4902ad7
wick-fe <wick-fe@gate.me>
2024-08-12 22:35:42 +0800

    feat: add bundle analyzer and lazying loading

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-const path = require("path");
-module.exports = {
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
+const path = require('path')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+const nextConfig = {
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    webpack: (config) => {
+        config.resolve.alias.canvas = false
 
-    return config;
-  },
-};
+        return config
+    },
+}
+module.exports = withBundleAnalyzer(nextConfig)
